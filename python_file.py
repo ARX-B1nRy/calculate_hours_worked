0084f6b83aaf8955664cb6dccfc30fac231d4a6b
ARX <125687857+ARX-B1nRy@users.noreply.github.com>
2024-02-26 15:06:51 -0500

    Create python_file.py

---
+++ python_file.py
@@ -0,0 +1,31 @@
+# pay rate python program
+
+user = print("How many employees to process:")
+user = int(input())
+counter = 1
+
+# while loop
+while counter <= user:
+# ask user for employee first name
+first_name = print("Enter employee first name: ")
+first_name = str(input())
+
+# ask user for employee last name
+last_name = print("Enter employee last name: ")
+last_name = str(input())
+
+# ask user for hours worked by employee
+hours_work = print("Enter the amount of hours", first_name, last_name , "worked: ")
+hours_work = int(input())
+
+# ask user employee pay rate
+pay_rate = print("Enter", first_name, last_name, "pay rate: ")
+pay_rate = int(input())
+
+# multiplies hours worked by pay rate to find employee salary
+pay = hours_work * pay_rate
+
+# prints everything out
+print(first_name, last_name, "worked", hours_work, "hours at a pay rate of $", pay_rate, "and made $", pay, "this week")
+counter += 1
+print("Out of loop")
