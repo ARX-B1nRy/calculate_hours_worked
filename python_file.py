200a9517f277676f6853d1f12e27cbb632f44627
ARX <125687857+ARX-B1nRy@users.noreply.github.com>
2024-02-26 15:18:20 -0500

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -7,25 +7,25 @@ counter = 1
 # while loop
 while counter <= user:
     # ask user for employee first name
-    first_name = print("Enter employee first name: ")
-    first_name = str(input())
+    fname = print("Enter employee first name: ")
+    fname = str(input())
 
     # ask user for employee last name
-    last_name = print("Enter employee last name: ")
-    last_name = str(input())
+    lname = print("Enter employee last name: ")
+    lname = str(input())
 
     # ask user for hours worked by employee
-    hours_work = print("Enter the amount of hours", first_name, last_name , "worked: ")
+    hours_work = print("Enter the amount of hours", fname, lname , "worked: ")
     hours_work = float(input())
 
     # ask user employee pay rate
-    pay_rate = print("Enter", first_name, last_name, "pay rate: ")
+    pay_rate = print("Enter", fname, lname, "pay rate: ")
     pay_rate = float(input())
     
     # multiplies hours worked by pay rate to find employee salary
     pay = hours_work * pay_rate
     
     # prints everything out
-    print(first_name, last_name, "worked", hours_work, "hours at a pay rate of $", pay_rate, "and made $", pay, "this week")
+    print(fname, lname, "worked", hours_work, "hours at a pay rate of $", pay_rate, "and made $", pay, "this week")
     counter += 1
     print("Program Completed")
